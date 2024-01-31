5ee56710e893134c561b6f91d392ea048140d34b
RbAvci <rb.avci@gmail.com>
2024-01-31 23:25:49 +0000

    add  template update with data

--- script.js
+++ script.js
@@ -6,7 +6,16 @@ function setup() {
 
 function makePageForEpisodes(episodeList) {
   const rootElem = document.getElementById("root");
-  rootElem.textContent = `Got ${episodeList.length} episode(s)`;
+  for (const episode of episodeList) {
+    const showCard = document.getElementById("show-card").content.cloneNode(true);
+
+    showCard.querySelector("#show-title").textContent = episode.name;
+    showCard.querySelector("#show-episode").textContent = `${episode.season} - ${episode.number}`;
+    showCard.querySelector("img").src = episode.image.medium;
+    showCard.querySelector("#show-summary").innerHTML = episode.summary;
+
+    rootElem.append(showCard);
+  }
 }
 
 window.onload = setup;
