1091c5e3a1831ef5d9c5c2aff30da7dea7faa3cf
RbAvci <rb.avci@gmail.com>
2024-02-12 23:35:57 +0000

    refactor for API fetch

--- script.js
+++ script.js
@@ -1,94 +1,109 @@
 const state = {
   episodes: getAllEpisodes(),
   searchTerm: "",
+  selectedEpisodeId: "",
 };
 
-function setup() {
-  makePageForEpisodes(state.episodes);
-  populateEpisodeDropdown(state.episodes);
-}
-
-function makePageForEpisodes(episodeList) {
-  const rootElem = document.getElementById("root");
-  for (const episode of episodeList) {
-    const showCard = document
-      .getElementById("show-card")
-      .content.cloneNode(true);
-    const showTitle = showCard.querySelector("#show-title");
-    showTitle.textContent = episode.name;
-    showTitle.setAttribute("href", episode.url);
-    showCard.querySelector("#show-episode").textContent = episodeCode(
-      episode.season,
-      episode.number
-    );
-    showCard.querySelector("img").src = episode.image.medium;
-    showCard.querySelector("#show-summary").innerHTML = episode.summary;
-    const episodeId = `episode-${episode.id}`;
-    showCard.querySelector(".card-temp").id = episodeId;
-
-    rootElem.append(showCard);
-    let allCards = [...document.querySelectorAll(".card-temp")];
-    searchResults(allCards);
-  }
+function createEpisodeCard(episode) {
+  const card = document.getElementById("episode-card").content.cloneNode(true);
+  const title = card.querySelector("#episode-title");
+  title.textContent = episode.name;
+  title.setAttribute("href", episode.url);
+  card.querySelector("#episode-code").textContent = episodeCode(
+    episode.season,
+    episode.number
+  );
+  card.querySelector("#episode-img").src = episode.image.medium;
+  card.querySelector("#episode-summary").innerHTML = episode.summary;
+  return card;
 }
 
 function episodeCode(season, number) {
   let s = season < 10 ? `0${season}` : season;
-  let n = number < 10 ? `0${number}` : number;
-  let code = `S${s}E${n}`;
+  let num = number < 10 ? `0${number}` : number;
+  let code = `S${s}E${num}`;
   return code;
 }
 
-const searchResults = (allCards) => {
-  const searchInput = document.getElementById("search");
+const searchBox = document.getElementById("search");
+searchBox.addEventListener("input", handleSearchInput);
 
-  searchInput.addEventListener("input", updateSearchTerm);
-
-  function updateSearchTerm(event) {
-    state.searchTerm = searchInput.value.toLowerCase();
-    console.log(state.searchTerm);
-    let numberOfEpisodesDisplayed = 0;
-    allCards.forEach(function (episode) {
-      if (episode.innerText.toLowerCase().includes(state.searchTerm)) {
-        episode.style.display = "block";
-        numberOfEpisodesDisplayed++;
-      } else {
-        episode.style.display = "none";
-      }
-      document.querySelector(
-        "#number-of-filtered-episodes"
-      ).innerHTML = `Displaying ${numberOfEpisodesDisplayed}/${allCards.length} episodes`;
-    });
-  }
-};
-
-function populateEpisodeDropdown(episodeList) {
-  const dropdown = document.getElementById("episodeDropdown");
-
-  episodeList.forEach((episode) => {
-    const option = document.createElement("option");
-    option.value = episode.id;
-    option.textContent = `${episodeCode(episode.season, episode.number)} -${episode.name} `;
-    dropdown.appendChild(option);
-  });
-  dropdown.addEventListener("change", handleDropdownChange);
+function handleSearchInput(event) {
+  state.searchTerm = event.target.value;
+  console.log(state.searchTerm);
+  renderBySearch();
 }
 
-function handleDropdownChange() {
-  const selectedEpisodeId = document.getElementById("episodeDropdown").value;
-  const selectedEpisode = state.episodes.find(
-    (episode) => episode.id === parseInt(selectedEpisodeId)
+const episodeSelector = document.getElementById("episode-selector");
+episodeSelector.addEventListener("change", handleSelect);
+
+function handleSelect(event) {
+  state.selectedEpisodeId = event.target.value;
+  console.log(state.selectedEpisodeId);
+  renderBySelect();
+}
+
+function createEpisodeListItem(episode) {
+  const episodeListItem = document
+    .getElementById("episode-list")
+    .content.cloneNode(true);
+  const option = episodeListItem.querySelector("option");
+  option.textContent = `${episodeCode(episode.season, episode.number)} - ${
+    episode.name
+  }`;
+  option.setAttribute("value", episode.id);
+
+  return episodeListItem;
+}
+
+function fillEpisodeList() {
+  const episodes = state.episodes;
+  for (e of episodes) {
+    const episodeListItem = createEpisodeListItem(e);
+    document.getElementById("episode-selector").append(episodeListItem);
+  }
+}
+
+function renderBySelect() {
+  searchBox.value = "";
+  state.searchTerm = "";
+  renderByFilter((episode) => state.selectedEpisodeId == episode.id);
+}
+
+function renderBySearch() {
+  episodeSelector.selectedIndex = 0;
+  state.selectedEpisodeId = "";
+  renderByFilter(filterBySearch);
+}
+
+function filterBySearch(episode) {
+  const lowercaseName = episode.name.toLowerCase();
+  const lowercaseSummary = episode.summary.toLowerCase();
+  return (
+    lowercaseName.includes(state.searchTerm.toLowerCase()) ||
+    lowercaseSummary.includes(state.searchTerm.toLowerCase())
   );
-  if (selectedEpisode) {
-    scrollToEpisode(selectedEpisode);
-  }
 }
 
-function scrollToEpisode(episode) {
-  const episodeId = `episode-${episode.id}`;
-  const episodeElement = document.getElementById(episodeId);
-  if (episodeElement) {
-    episodeElement.scrollIntoView({ behavior: "smooth" });
-  }
+function renderByFilter(filterFunction) {
+  const rootElem = document.getElementById("root");
+  rootElem.innerHTML = "";
+
+  const filteredEpisodes = state.episodes.filter(filterFunction);
+  const episodeCards = filteredEpisodes.map(createEpisodeCard);
+  rootElem.append(...episodeCards);
+
+  document.getElementById(
+    "filter-info"
+  ).textContent = `Displaying ${filteredEpisodes.length}/${state.episodes.length} episodes`;
 }
-window.onload = setup;
+
+document.getElementById("all-episodes").addEventListener("click", render);
+
+function render() {
+  fillEpisodeList();
+  renderBySelect();
+  renderBySearch();
+}
+
+render();
